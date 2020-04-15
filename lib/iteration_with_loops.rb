bb120a1d6b21dc861fc983bea53ff10bbe95c5fc
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-15 02:18:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,6 +4,7 @@ def join_nested_strings(src)
   
   strings_array = []
   row_index = 0
+  
   while row_index < src.length do
     element_index = 0
     while element_index < src[row_index].length do
@@ -12,13 +13,11 @@ def join_nested_strings(src)
       end
       element_index += 1
     end
-    
-    
     row_index += 1
   end
   
-  
-  
+  new_string = strings_array.join(" ")
+  new_string
   
   
 end
